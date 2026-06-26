740cf90d775d92f505a8ddc3a3e73a3661c22f98
shadmanadman <adman.shadman@gmail.com>
2026-06-26 10:43:10 +0100

    Impl core module

---
+++ core/src/iosMain/kotlin/org/kmp/playground/kflite/core/Utils.kt
@@ -0,0 +1,17 @@
+package org.kmp.playground.kflite.core
+
+import kotlinx.cinterop.*
+import platform.Foundation.NSErrorVar
+
+@OptIn(ExperimentalForeignApi::class)
+fun <T> errorHandled(block: (CPointer<CPointerVar<NSErrorVar>>?) -> T): T {
+    memScoped {
+        val errorPtr = alloc<CPointerVar<NSErrorVar>>()
+        val result = block(errorPtr.ptr)
+        val error = errorPtr.value
+        if (error != null) {
+            throw Exception(error.localizedDescription)
+        }
+        return result
+    }
+}
