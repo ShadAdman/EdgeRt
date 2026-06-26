4249f9f30231c83534b48de2f79a0a8c82b0e73b
shadmanadman <adman.shadman@gmail.com>
2026-06-26 10:35:21 +0100

    Impl postprocessing module

---
+++ postprocessing/coco/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget()
+    iosX64(); iosArm64(); iosSimulatorArm64()
+    sourceSets {
+        commonMain.dependencies { implementation(project(":core")) }
+    }
+}
+
+android {
+    namespace = "org.kmp.playground.kflite.postprocessing.coco"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+}
