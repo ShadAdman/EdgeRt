14b8a73da10a1af6f4b6fbab8d38176b3712c7d0
shadmanadman <adman.shadman@gmail.com>
2026-06-28 18:59:37 +0100

    Fix memmory mapping

--- core/src/commonMain/kotlin/org/kmp/playground/kflite/core/delegate/Delegate.kt
+++
@@ -1,3 +0,0 @@
-package org.kmp.playground.kflite.core.delegate
-
-interface Delegate
--- core/src/commonMain/kotlin/org/kmp/playground/kflite/core/model/KFliteModel.kt
+++
@@ -1,6 +0,0 @@
-package org.kmp.playground.kflite.core.model
-
-object KFliteModel {
-    fun fromBytes(bytes: ByteArray): ModelSource = ByteArraySource(bytes)
-    fun fromFile(path: String): ModelSource = FileSource(path)
-}
