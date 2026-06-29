9e58d224866baba6f2ac57ce0990e63b6e9e8879
shadmanadman <adman.shadman@gmail.com>
2026-06-29 15:34:42 +0100

    Fix missing imports

--- kflite/src/iosMain/kotlin/org/kmp/playground/kflite/tensor/Tensor.iOS.kt
+++ kflite/src/iosMain/kotlin/org/kmp/playground/kflite/tensor/Tensor.iOS.kt
@@ -6,9 +6,10 @@ import org.kmp.playground.kflite.kflite.*
 import org.kmp.playground.kflite.model.*
 import org.kmp.playground.kflite.tensor.*
 
+@OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
 actual class Tensor(private val runtimeTensor: PlatformTensor) {
     actual val dataType: TensorDataType get() = TensorDataType.FLOAT32 // FIXME
-    actual val name: String get() = runtimeTensor.name() ?: ""
+    actual val name: String get() = "" // FIXME
     actual val shape: IntArray get() = IntArray(0) // FIXME
 }
 
