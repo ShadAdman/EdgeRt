994974f9f49229a2e21d8e318140d702ba7e9cd2
shadmanadman <adman.shadman@gmail.com>
2026-06-29 11:04:25 +0100

    remove extra step

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinCocoapods) apply false
     alias(libs.plugins.vanniktechPublish) apply false
-    id("signing")
 }
 
 subprojects {
