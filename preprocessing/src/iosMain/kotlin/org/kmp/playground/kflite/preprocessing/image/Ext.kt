fffd8b1bd69124106066c7216de7d123db339113
shadmanadman <adman.shadman@gmail.com>
2026-06-29 12:32:34 +0100

    Fix missing imports

--- preprocessing/src/iosMain/kotlin/org/kmp/playground/kflite/preprocessing/image/Ext.kt
+++ preprocessing/src/iosMain/kotlin/org/kmp/playground/kflite/preprocessing/image/Ext.kt
@@ -93,3 +93,59 @@ internal fun ImageBitmap.toUIImage(): UIImage? {
 
     return uiImage
 }
+
+
+@OptIn(ExperimentalForeignApi::class)
+fun UIImage.toRGBByteArray(normalize: Boolean): ByteArray {
+    var width = 0
+    var height = 0
+    this.size.useContents {
+        width = this.width.roundToInt()
+        height = this.height.roundToInt()
+    }
+
+    val bytesPerPixel = 4
+    val byteCount = width * height * bytesPerPixel
+
+    val colorSpace = CGColorSpaceCreateDeviceRGB()
+    val rawData = ByteArray(byteCount)
+    rawData.usePinned { pinned ->
+        val context = CGBitmapContextCreate(
+            pinned.addressOf(0),
+            width.toULong(),
+            height.toULong(),
+            8.toULong(),
+            (bytesPerPixel * width).toULong(),
+            colorSpace,
+            CGImageAlphaInfo.kCGImageAlphaPremultipliedLast.toUInt()
+        ) ?: error("Could not create context")
+
+        CGContextDrawImage(context, CGRectMake(0.0, 0.0, width.toDouble(), height.toDouble()), this.CGImage())
+
+        val result = mutableListOf<Byte>()
+        for (i in 0 until byteCount step bytesPerPixel) {
+            val r = rawData[i].toUByte().toInt()
+            val g = rawData[i + 1].toUByte().toInt()
+            val b = rawData[i + 2].toUByte().toInt()
+            if (normalize) {
+                result.addAll(listOf(
+                    (r / 255.0f).toBits().toByteList(),
+                    (g / 255.0f).toBits().toByteList(),
+                    (b / 255.0f).toBits().toByteList()
+                ).flatten())
+            } else {
+                result.add(r.toByte())
+                result.add(g.toByte())
+                result.add(b.toByte())
+            }
+        }
+        return result.toByteArray()
+    }
+}
+
+fun Int.toByteList(): List<Byte> = listOf(
+    (this and 0xFF).toByte(),
+    ((this shr 8) and 0xFF).toByte(),
+    ((this shr 16) and 0xFF).toByte(),
+    ((this shr 24) and 0xFF).toByte()
+)
