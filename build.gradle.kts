5e58ba1044ecaafdeef28e94746845ae2144b623
shadmanadman <adman.shadman@gmail.com>
2026-07-08 19:51:03 +0100

    Fix not using artifact id

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 subprojects {
     pluginManager.withPlugin("com.vanniktech.maven.publish") {
         val props = project.providers
-        val artifactIdProp = props.gradleProperty("POM_ARTIFACT_ID").get()
+        val artifactIdProp = props.gradleProperty("POM_ARTIFACT_ID").orNull
         val snapshotVersion = props.gradleProperty("PROJECT_VERSION_NAME").get()
         val computedVersion = System.getenv("GITHUB_REF")
             ?.takeIf { it.startsWith("refs/tags/") }
@@ -29,6 +29,7 @@ subprojects {
 
             publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
             signAllPublications()
+
 
             coordinates(project.group.toString(), artifactIdProp, project.version.toString())
 
