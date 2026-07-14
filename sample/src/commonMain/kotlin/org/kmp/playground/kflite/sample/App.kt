e7509b49893a95183b792e3b1a7dada44db52bf1
shadmanadman <adman.shadman@gmail.com>
2026-07-14 13:07:34 +0330

    Impl face detection example

--- sample/src/commonMain/kotlin/org/kmp/playground/kflite/sample/App.kt
+++ sample/src/commonMain/kotlin/org/kmp/playground/kflite/sample/App.kt
@@ -1,11 +1,17 @@
 package org.kmp.playground.kflite.sample
 
 import androidx.compose.runtime.Composable
+import org.kmp.playground.kflite.sample.face_detection.BlazeFaceLiteRTSample
+import org.kmp.playground.kflite.sample.face_detection.BlazeFaceTFLiteSample
 import org.kmp.playground.kflite.sample.object_detection.litert.ResourceRunModelSample
 
 @Composable
 fun App() {
     // Note: For memory-mapped resource loading on Android, we need the full asset path.
     // ResourceRunModelSample avoids copying bytes by mapping from bundle/assets.
-    ResourceRunModelSample("composeResources/kflitelib.sample.generated.resources/files/efficientdet-lite2.tflite")
+    // ResourceRunModelSample("composeResources/kflitelib.sample.generated.resources/files/efficientdet-lite2.tflite")
+
+    // BlazeFace Face Detection Examples
+    BlazeFaceTFLiteSample()
+    // BlazeFaceLiteRTSample()
 }
